e7db17675852cca72a4ac555b81ebc4d5d352193
KRSBX <firdaus.sati50@gmail.com>
2020-12-21 19:33:43 +0700

    Adding lot of things
    Add asking function for asking person/object
    Add responds for non defined command
    Split things to other functions
    Add WishMe Functions

--- Genesis.py
+++ Genesis.py
@@ -1,58 +1,122 @@
 import speech_recognition as sr #Voice Recog
 import pyttsx3 # TTS
-from datetime import datetime
-import sys
-# import pywhatkit
+from datetime import datetime #For Time Purposes
+import os
+import pywhatkit #Play Youtube
+import webbrowser #For Searching Purposes
+import wikipedia #Summary Informations
 
-tts = pyttsx3.init()
+tts = pyttsx3.init('sapi5')
 listener = sr.Recognizer()
+listener.pause_threshold = 0.5
+
+querys = ["who is", "what is", "time", "date", "play", "weather"]
+wisher = ["Good Morning Sir!", "Good Afternoon Sir!", "Good Night Sir!"]
 
 def Talk(txt):
-    print(txt)
     tts.say(txt)
     tts.runAndWait()
 
-def CommandLine(command):
-    if 'time' in command:
-        Talk("Current time is "+datetime.now().strftime('%I:%M'))
-    if 'day' in command:
-        Talk("Current date is "+datetime.today.strftime('%d %B %Y'))
-    if 'play' in command:
-        song = command.replace('play', '')
-        Talk("Playing "+song)
-        # pywhatkit.playonyt(song)
+#For All Query That Used
+def CheckQuery(command):
+    for query in querys:
+        if query in command:
+            return True
 
-    TakeCommand(0)
+#For Certain Query of strings
+def NewCheckQuery(querys, command):
+    for query in querys:
+        if query in command:
+            return True
+
+def CommandLine(command):
+    if CheckQuery(command):
+        if NewCheckQuery(["time", "date", "weather"], command):
+            if 'time' in command:
+                CurrentTime()
+            if 'date' in command:
+                CurrentDate()
+            if 'weather' in command:
+                CurrentWeather(command)
+        if not NewCheckQuery(["time", "date", "weather"], command):
+            if 'who is' in command:
+                SearchWho(command)
+            if 'what is' in command:
+                SearchWhat(command)
+        if 'play' in command:
+            PlayYoutube(command)
+    else:
+        Talk("Command not found")
+
+def CurrentTime():
+    Talk("Current time is "+datetime.now().strftime('%I:%M'))
+
+def CurrentDate():
+    Talk("Current date is "+datetime.today().strftime('%d %B %Y'))
+
+def CurrentWeather(command):
+    webbrowser.get().open_new_tab("https://google.com/search?q="+command)
+    Talk("Here is the info of the current wheather")
+
+def SearchWho(command):
+    search = command.replace('who is', '', 1)
+    info = wikipedia.summary(search, 1)
+    # pywhatkit.search(command)
+    webbrowser.get().open_new_tab("https://google.com/search?q="+command)
+    Talk(info)
+
+def SearchWhat(command):
+    search = command.replace('what is', '', 1)
+    info = wikipedia.summary(search, 1)
+    # pywhatkit.search(command)
+    webbrowser.get().open_new_tab("https://google.com/search?q="+command)
+    Talk(info)
+
+def PlayYoutube(command):
+    song = command.replace('play', '', 1)
+    Talk("Playing"+song)
+    pywhatkit.playonyt(song)
 
 def TakeCommand(check):
     if(check):
+        print("I'm Listenning...")
         Talk("I'm Listenning...")
     else:
         print("Call Me Please...")
 
     try:
         with sr.Microphone() as source:
-            command = listener.recognize_google(listener.listen(source)).lower()
-            if(check != 1):
-                if 'genesis' in command: #Check If It Calls
-                    TakeCommand(1) #Trigger The Command To Take Command
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice).lower()
+        if check == 0:
+            if 'genesis' in command: #Check If It Calls
+                TakeCommand(1) #Trigger The Command To Take Command
+        else:
+            if "i'm good" in command:
+                Talk("See you later sir")
             else:
-                if "i'm good" in command:
-                    Talk("See you later")
-                else:
-                    CommandLine(command.replace('genesis',''))
+                print(command)
+                CommandLine(command)
     except:
-        if(check):
+        if check == 1:
             if "i'm good" not in command:
                 Talk("Can't catch that one")
-                TakeCommand(0)
-        else:
-            TakeCommand(0)
 
-    if(check):
-        if "i'm good" not in command:
-            TakeCommand(0)
+    TakeCommand(0)
+
+def WishMe():
+    hour = datetime.now().hour
+
+    if hour >= 0 and hour < 12:
+        print(wisher[0])
+        Talk(wisher[0])
+    elif hour >= 12 and hour < 18:
+        print(wisher[1])
+        Talk(wisher[1])
     else:
-        TakeCommand(0)
+        print(wisher[2])
+        Talk(wisher[2])
 
+WishMe()
+Talk("Genesis is ready!")
 TakeCommand(0)
